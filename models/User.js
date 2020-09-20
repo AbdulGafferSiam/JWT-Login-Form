f45effc06f33bbd5beba68b9e9aa98b9e063ac95
unknown <md.abdul.gaffer.siam@kanto.sysystem>
2020-09-20 16:44:16 +0900

    Mongoose Hooks

--- models/User.js
+++ models/User.js
@@ -16,6 +16,22 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+// fire a function after doc saved to db / after save event
+// we need to use next for any mongoose middleware or hooks
+// doc refer to the saved instance
+userSchema.post('save', (doc, next) => {
+    console.log('new user created and saved', doc);
+    next();
+});
+
+// fire a function after doc saved to db / after save event
+// not use arrow function here to use 'this' keyword
+// 'this' refer to the instance of the user we trying to create
+userSchema.pre('save', function (next) {
+    console.log('user about to be created and saved', this);
+    next();
+})
+
 // here the model 'user' need to be singular format
 // mongoose make it plural and save data on to the collection 'users'
 const User = mongoose.model('user', userSchema);
