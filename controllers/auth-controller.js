618a994eefc3d11ae5617dd924c8bfc85c0a3b80
unknown <md.abdul.gaffer.siam@kanto.sysystem>
2020-09-20 19:40:47 +0900

    Logging user in part-2

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -3,8 +3,20 @@ const jwt = require('jsonwebtoken');
 
 // handle errors
 const handleErrors = (err) => {
-    // console.log(err.message, err.code);
+    console.log(err.message, err.code);
     let errors = { email: '', password: '' };
+
+    // incorrect email
+    if (err.message === 'Incorrect Email') {
+        errors.email = 'Email is not registered';
+        return errors;
+    }
+
+    // incorrect password
+    if (err.message === 'Incorrect Password') {
+        errors.password = 'Password is incorrect';
+        return errors;
+    }
 
     // duplicate error code
     if (err.code === 11000) {
@@ -35,7 +47,6 @@ const createToken = (id) => {
         expiresIn: maxAge
     })
 }
-
 
 module.exports.signup_get = (req, res) => {
     res.render('signup');
@@ -75,10 +86,16 @@ module.exports.login_post = async (req, res) => {
     
     try {
         const user = await User.login(email, password);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, {
+            httpOnly: true,
+            maxAge: maxAge * 1000
+        })
         // send json to front end
         res.status(200).json({ user: user._id });
     } catch (err) {
+        const errors = handleErrors(err);
         // send json to front end
-        res.status(400).json({});
+        res.status(400).json({ errors });
     }
 }
